e429fad81b05bbf0840487f3c85a803015c60ebd
Solomon Umeh <solomonu@xceed365.com>
2020-04-10 04:15:31 +0100

    committed

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/query/dist')))
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
